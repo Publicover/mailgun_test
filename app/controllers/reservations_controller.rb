96000684f17d668ec7a7c512e903e2702760fe07
Jim Publicover <jameslpublicover@gmail.com>
2016-10-23 11:02:01 -0400

    controllers, views and forms up and running

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,52 @@
 class ReservationsController < ApplicationController
+
+  def index
+    @reservations = Reservation.all
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def edit
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      redirect_to @reservation
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+
+    if @reservation.update(reservation_params)
+      redirect_to @reservation
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @reservaion = Reservation.find(params[:id])
+    @reservaion.destroy
+
+    redirect_to reservations_path
+  end
+
+  private
+
+    def reservation_params
+      params.require(:reservation).permit(:f_name, :l_name)
+    end
+
 end
