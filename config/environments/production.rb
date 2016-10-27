6f2a8cf3f55f5f36fed1de87c12abbd9d63e1314
Jim Publicover <jameslpublicover@gmail.com>
2016-10-27 17:39:24 -0400

    change language in config/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,15 +60,15 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'fulltestsuite.herokuapp.com', #eg: 'yourappname.herokuapp.com'
     :authentication => :plain,
   }
-  ActionMailer::Base.delivery_method = :smtp
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   address:              'smtp.mailgun.org',
